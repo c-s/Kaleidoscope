b37076fef6feba13902e2f7ef4b81c56bf8bd197
c-s <cs.p@outlook.com>
2014-05-03 21:00:32 -0400

    Initial Commit

---
+++ Kaleidoscope/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Kaleidoscope
+//
+//  Created by Chang-Soon on 5/3/14.
+//  Copyright (c) 2014 Chang-Soon. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
