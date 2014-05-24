e9f3a68559b17ec8d0a3b3f886058e4b95696332
c-s <cs.p@outlook.com>
2014-05-24 17:44:50 -0400

    test

--- Kaleidoscope/main.cpp
+++ Kaleidoscope/main.cpp
@@ -6,7 +6,38 @@
 //  Copyright (c) 2014 Chang-Soon. All rights reserved.
 //
 
-#include <iostream>
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <string>
+#include <vector>
+
+enum class Token {
+    eof = -1,
+    def = -2,
+    extrn = -3,
+    identifier = -4,
+    number = -5
+};
+
+static std::string IdentifierStr;
+static double NumVal;
+
+static int gettok() {
+    static int LastChar = ' ';
+    
+    while(isspace(LastChar))
+        LastChar = getchar();
+    
+    if(isalpha(LastChar)) {
+        IdentifierStr = LastChar;
+        while (isalnum((LastChar = getchar())))
+            IdentifierStr += LastChar;
+    }
+}
+
+
 
 int main(int argc, const char * argv[])
 {
