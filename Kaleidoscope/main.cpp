2ee7d85e34e0313735b409c6f4ac509bc3fd5ced
C. S. Park <cs.p@outlook.com>
2014-05-25 21:38:04 -0400

    working on (probably chapter 2)

--- Kaleidoscope/main.cpp
+++ Kaleidoscope/main.cpp
@@ -12,14 +12,17 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <boost/variant/variant.hpp>
 
-enum class Token {
+enum class SpecialToken {
     eof = -1,
     def = -2,
     extrn = -3,
     identifier = -4,
     number = -5
 };
+
+using Token = boost::variant<SpecialToken, int>;
 
 static std::string IdentifierStr;
 static double NumVal;
@@ -34,9 +37,9 @@ static Token gettok() {
         IdentifierStr = LastChar;
         while(isalnum((LastChar = getchar())))
             IdentifierStr += LastChar;
-        if(IdentifierStr == "def") return Token::def;
-        if(IdentifierStr == "extern") return Token::extrn;
-        return Token::identifier;
+        if(IdentifierStr == "def") return SpecialToken::def;
+        if(IdentifierStr == "extern") return SpecialToken::extrn;
+        return SpecialToken::identifier;
     }
     
     if(isdigit(LastChar) || LastChar == '.') {
@@ -47,7 +50,7 @@ static Token gettok() {
         } while(isdigit(LastChar) || LastChar == '.');
         
         NumVal = strtod(NumStr.c_str(), 0);
-        return Token::number;
+        return SpecialToken::number;
     }
     
     if(LastChar == '#') {
@@ -59,12 +62,62 @@ static Token gettok() {
     }
     
     if(LastChar == EOF)
-        return Token::eof;
+        return SpecialToken::eof;
     
     int ThisChar = LastChar;
     LastChar = getchar();
     return ThisChar;
 }
+
+class ExprAST {
+public:
+    virtual ~ExprAST() {}
+};
+
+class NumberExprAST : public ExprAST {
+    double Val;
+public:
+    NumberExprAST(double val) : Val {val} {}
+};
+
+class VariableExprAST : public ExprAST {
+    std::string Name;
+public:
+    VariableExprAST(const std::string &name) : Name {name} {}
+};
+
+class BinaryExprAST : public ExprAST {
+    char Op;
+    ExprAST *LHS, *RHS;
+public:
+    BinaryExprAST(char op, ExprAST *lhs, ExprAST *rhs)
+    : Op{op}, LHS {lhs}, RHS {rhs} {}
+};
+
+class CallExprAST : public ExprAST {
+    std:: string Callee;
+    std::vector<ExprAST*> Args;
+public:
+    CallExprAST(const std::string &callee, std::vector<ExprAST*> &args)
+    : Callee {callee}, Args {args} {}
+};
+
+class PrototypeAST {
+    std::string Name;
+    std::vector<std::string> Args;
+    
+public:
+    PrototypeAST(const std::string &name, const std::vector<std::string> &args)
+    : Name {name}, Args {args} {}
+};
+
+class FunctionAST {
+    PrototypeAST *Proto;
+    ExprAST *Body;
+public:
+    FunctionAST(PrototypeAST *proto, ExprAST *body)
+    : Proto {proto}, Body {body} {}
+};
 
 
 
